24079229a339db16440dfb77ebe249101013ac78
bordemof <lonami80@gmail.com>
2015-12-29 19:09:41 +0100

    Added s3 caching service

---
+++ services/s3.js
@@ -0,0 +1,59 @@
+"use strict"
+const
+    knox   = require('knox'),
+    Hope   = require('hope'),
+    client = knox.createClient({
+        key   : 'YOUR-S3-KEY',
+        secret: 'YOUR-S3-SECRET-KEY',
+        bucket: 'YOUR-S3-BUCKET'
+    });
+
+
+var s3wrapper = function(undefined) {
+
+    var priority = 0;
+
+    var _getPageIfCached = function(hash) {
+        var promise = new Hope.Promise();
+        console.log("Searching in cache for ", hash+'.html')
+        client.get(hash+'.html').on('response', function(res){
+              var body = '';
+              res.on('data', function(chunk){
+                 body += chunk;
+              });
+
+              res.setEncoding('utf8');
+
+              res.on('end', function(){
+                if (res.statusCode === 200) {
+                    promise.done(null,body)
+                } else {
+                    promise.done(true,null)
+                }
+              });
+        }).end();
+
+        return promise;
+
+    };
+
+    var _storePage = function(hash, html) {
+        var string = html;
+        var req = client.put(hash+'.html', {
+            'Content-Length': Buffer.byteLength(string)
+          , 'Content-Type'  : 'document'
+        });
+        req.on('response', function(res){
+          if (200 == res.statusCode) { console.log('saved to %s', req.url); }
+        });
+        req.end(string);
+    };
+
+    return {
+        getPageIfCached   : _getPageIfCached,
+        storePage         : _storePage
+    };
+
+}
+
+module.exports = s3wrapper;
