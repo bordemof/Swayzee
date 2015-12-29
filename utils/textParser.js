1f35bc6c40d86352ed35414d6446b26689034966
bordemof <lonami80@gmail.com>
2015-12-29 19:07:41 +0100

    Extracted some utils

---
+++ utils/textParser.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var textParser = function(undefined) {
+
+    var SCRIPT_REGEX   = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+
+    // Removes all script tags from the html
+    var removeScriptTags = function(html) {
+        while (SCRIPT_REGEX.test(html)) { html = html.replace(SCRIPT_REGEX, ""); }
+        return html;
+    };
+
+    // Extracts the hash and adds back the #! convention
+    var extractHashURL = function(url) {
+        return  '#!'+url.split("_escaped_fragment_=")[1];
+    };
+
+    return {
+        extractHashURL   : extractHashURL,
+        removeScriptTags : removeScriptTags
+    }
+}
+
+module.exports = textParser();
